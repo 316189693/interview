cabd9aaa136ff4b5f3d59c9b166892a6d2444d51
will zheng <will.zheng@unisco.com>
2020-07-16 13:52:31 +0800

    ff

--- src/main/java/com/interview/Utils.java
+++ src/main/java/com/interview/Utils.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 
 public class Utils {
@@ -18,17 +18,16 @@ public class Utils {
         Assertions.assertNotNull(extensions, "extensions list is null");
         return extensions.parallelStream().sorted(
                 ExtensionUtils.<Extension>compare()
-                .thenComparing((o1,o2)->o2.getFirstName().compareTo(o1.getFirstName()))
-                .thenComparing((o1,o2)->StringUtils.trimStr(o2.getLastName()).compareTo(StringUtils.trimStr(o1.getLastName())))
-                .thenComparing((o1,o2)->StringUtils.trimStr(o2.getExt()).compareTo(StringUtils.trimStr(o1.getExt())))
+                        .thenComparing((o1, o2) -> o2.getFirstName().compareTo(o1.getFirstName()))
+                        .thenComparing((o1, o2) -> StringUtils.trimStr(o2.getLastName()).compareTo(StringUtils.trimStr(o1.getLastName())))
+                        .thenComparing((o1, o2) -> StringUtils.trimStr(o2.getExt()).compareTo(StringUtils.trimStr(o1.getExt())))
         ).collect(Collectors.toList());
     }
-
 
     //Question2
     public static List<Extension> sortByExtType(List<Extension> extensions) {
         Assertions.assertNotNull(extensions, "extensions list is null");
-        return extensions.parallelStream().sorted(Comparator.comparing((o)->ExtTypeOrder.getOrderNumByExtType(o.getExtType()))).collect(Collectors.toList());
+        return extensions.parallelStream().sorted(Comparator.comparing((o) -> ExtTypeOrder.getOrderNumByExtType(o.getExtType()))).collect(Collectors.toList());
     }
 
     //Question3
@@ -36,29 +35,24 @@ public class Utils {
         Assertions.assertNotNull(saleItems, "saleItems list is null");
         Map<Integer, Double> map = saleItems.stream()
                 .collect(Collectors.groupingBy(Utils::getQuarter, Collectors.summingDouble(SaleItem::getSaleNumbers)));
-        return mapToList(map, (o)->o);
+        return mapToList(map, (o) -> o);
     }
 
-    private static int getQuarter(SaleItem saleItem) {
-        return saleItem.getMonth() % 3 > 0 ? (saleItem.getMonth() / 3 + 1) : (saleItem.getMonth() / 3);
-    }
-
-    private static <T> List<QuarterSalesItem>  mapToList(Map<Integer, T> map, Function<T, Double> mapper){
-        return  map.entrySet().stream().map(o->{
-            QuarterSalesItem quarterSalesItem = new QuarterSalesItem();
-            quarterSalesItem.setQuarter(o.getKey());
-            quarterSalesItem.setTotal(mapper.apply(o.getValue()));
-            return quarterSalesItem;}).collect(Collectors.toList());
-    }
     //Question4
     public static List<QuarterSalesItem> maxByQuarter(List<SaleItem> saleItems) {
         Assertions.assertNotNull(saleItems);
         Map<Integer, Optional<SaleItem>> map = saleItems.parallelStream()
                 .collect(Collectors.groupingBy(Utils::getQuarter, Collectors.maxBy(Comparator.comparingDouble(SaleItem::getSaleNumbers))));
-        return mapToList(map, (o)->o.get().getSaleNumbers());
+        return mapToList(map, (o) -> {
+            if (null == o.get()) {
+                return 0;
+            }
+            return o.get().getSaleNumbers();
+        });
     }
 
     //Question5
+
     /**
      * We have all Keys: 0-9;
      * usedKeys is an array to store all used keys like :[2,3,4];
@@ -68,6 +62,19 @@ public class Utils {
     public static int[] getUnUsedKeys(int[] allKeys, int[] usedKeys) {
         Assertions.assertNotNull(allKeys);
         Assertions.assertNotNull(usedKeys);
-        return Arrays.stream(allKeys).filter(o->!Arrays.stream(usedKeys).anyMatch(t->t==o)).toArray();
+        return Arrays.stream(allKeys).filter(o -> !Arrays.stream(usedKeys).anyMatch(t -> t == o)).toArray();
+    }
+
+    private static int getQuarter(SaleItem saleItem) {
+        return saleItem.getMonth() % 3 > 0 ? (saleItem.getMonth() / 3 + 1) : (saleItem.getMonth() / 3);
+    }
+
+    private static <T> List<QuarterSalesItem> mapToList(Map<Integer, T> map, ToDoubleFunction<T> mapper) {
+        return map.entrySet().stream().map(o -> {
+            QuarterSalesItem quarterSalesItem = new QuarterSalesItem();
+            quarterSalesItem.setQuarter(o.getKey());
+            quarterSalesItem.setTotal(mapper.applyAsDouble(o.getValue()));
+            return quarterSalesItem;
+        }).collect(Collectors.toList());
     }
 }
