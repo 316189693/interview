b47949a12068a448dbdcd617ff3f7cc9a3a3b153
willz <316189693@qq.com>
2021-09-06 21:10:21 +0800

    fff

--- src/main/java/com/interview/Utils.java
+++ src/main/java/com/interview/Utils.java
@@ -2,7 +2,6 @@ package com.interview;
 
 
 import java.util.*;
-import java.util.function.Supplier;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -37,7 +36,7 @@ public class Utils {
                 .collect(
                         Collectors.collectingAndThen(
                                 Collectors.groupingBy(Utils::getQuarter,
-                                                      Collectors.summingDouble(SaleItem::getSaleNumbers)
+                                        Collectors.summingDouble(SaleItem::getSaleNumbers)
                                 ),
                                 (obj) -> mapToList(obj, (o) -> o)
                         )
@@ -90,15 +89,15 @@ public class Utils {
 
     // found first value from list [3,5,6,7,8], expected is big or equal than 6
 
-    public static int fetchValue(List<Integer> numList) throws Exception{
-       return numList.stream().filter(item->item >= 6).findFirst().orElseThrow(()-> new Exception("Not Fund"));
+    public static int fetchValue(List<Integer> numList) throws Exception {
+        return numList.stream().filter(item -> item >= 6).findFirst().orElseThrow(() -> new Exception("Not Fund"));
     }
-   // found string 'ddddhhhhhfffkkkk' max char
+
+    // found string 'ddddhhhhhfffkkkk' max char
     public static String maxStr(String testStr) {
         return Stream.of(testStr.split("")).collect(
-                              Collectors.groupingBy((item)->item,
-                                      Collectors.counting())
-        ).entrySet().stream().max((a,b)-> (a.getValue()-b.getValue()) > 0 ? 1 : -1).get().getKey();
-
+                Collectors.groupingBy((item) -> item,
+                        Collectors.counting())
+        ).entrySet().stream().max((a, b) -> (a.getValue() - b.getValue()) > 0 ? 1 : -1).get().getKey();
     }
 }
